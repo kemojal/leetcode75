d410f8219eb1cf90fbfd728ade4e32d095032ded
kemo <kemo3855@yahoo.com>
2024-05-14 15:32:46 +0800

    leet code

--- LeetCode75.ts
+++ LeetCode75.ts
@@ -468,20 +468,55 @@ s and t consist only of lowercase English letters.
 Follow up: Suppose there are lots of incoming s, say s1, s2, ..., sk where k >= 109, and you want to check one by one to see if t has its subsequence. In this scenario, how would you change your code?
  */
 
-function isSubsequence(s: string, t: string): boolean {
-  let sPointer = 0;
-  let tPointer = 0;
-  while (sPointer < s.length && tPointer < t.length) {
-    if (s[sPointer] === t[tPointer]) {
-      sPointer++;
-    }
-    tPointer++;
-  }
+// function isSubsequence(s: string, t: string): boolean {
+//   let sPointer = 0;
+//   let tPointer = 0;
+//   while (sPointer < s.length && tPointer < t.length) {
+//     if (s[sPointer] === t[tPointer]) {
+//       sPointer++;
+//     }
+//     tPointer++;
+//   }
 
-  return sPointer === s.length;
-}
+//   return sPointer === s.length;
+// }
 
-let s = "abc",
-  t = "ahbgdc";
+// let s = "abc",
+//   t = "ahbgdc";
 
-console.log("isSubsequence: " + isSubsequence(s, t));
+// console.log("isSubsequence: " + isSubsequence(s, t));
+
+
+
+/**
+ * 
+ * You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+
+Notice that you may not slant the container.
+
+ 
+
+Example 1:
+
+
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. 
+In this case, the max area of water (blue section) the container can contain is 49.
+Example 2:
+
+Input: height = [1,1]
+Output: 1
+ 
+
+Constraints:
+
+n == height.length
+2 <= n <= 105
+0 <= height[i] <= 104
+ */
+
